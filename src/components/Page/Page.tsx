4fc6d0986bff3c612b3c15cf2aaeb0d4c27d4d62
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-10-26 00:16:26 +0200

    create Page component

---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+
+const Page = () => {
+
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
+
+export default Page
