013483bb495b6a00168c0c48d78df1b827d9d0d3
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-10-26 00:19:33 +0200

    created Main component and Jumbotron component. Added images

---
+++ src/components/Main/Jumbotron/Jumbotron.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './jumbotron.scss'
+
+const Navbar = () => {
+    return (
+        <section id="jumbotron">
+            <div className="header-after" />
+
+            <div className="bubble">
+rghte
+            </div>
+        </section>
+    )
+}
+
+export default Navbar
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './main.scss'
+import Jumbotron from "./Jumbotron/Jumbotron";
+
+const Main = () => {
+
+    return (
+        <main id="main">
+            <Jumbotron />
+        </main>
+    )
+}
+
+export default Main
