4e5fe4a08e11f5936fdfdb552539576d9f26032a
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-10-26 19:52:30 +0200

    added content and made it responsive

--- src/components/Main/Jumbotron/Jumbotron.tsx
+++ src/components/Main/Jumbotron/Jumbotron.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import './jumbotron.scss'
+import Text from "../../reusable/Text/Text"
 
 const Navbar = () => {
     return (
         <section id="jumbotron">
             <div className="header-after" />
-
-            <div className="bubble">
-rghte
+            <div className="content">
+                <div className="bubble">
+                    <Text type={"h2"}>Testo</Text>
+                    <Text type={"h1"} color={"#fe5d37"}>Testo</Text>
+                    <Text type={"h2"}>Testo</Text>
+                </div>
             </div>
         </section>
     )
