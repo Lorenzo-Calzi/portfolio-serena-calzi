c9b443950bf24093dc09d3f0a1a30ad5d6add273
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-10-27 17:35:23 +0200

    added hero animation and bubble images

--- src/components/Main/Jumbotron/Jumbotron.tsx
+++ src/components/Main/Jumbotron/Jumbotron.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import './jumbotron.scss'
 import Text from "../../reusable/Text/Text"
+import LeftLineImg from '../../../assets/images/hero-left-white-line.png'
+import RightLineImg from '../../../assets/images/hero-right-white-line.png'
+import CarImg from '../../../assets/images/hero-car.png'
+
 
 const Navbar = () => {
     return (
@@ -11,6 +15,11 @@ const Navbar = () => {
                     <Text type={"h2"}>Testo</Text>
                     <Text type={"h1"} color={"#fe5d37"}>Testo</Text>
                     <Text type={"h2"}>Testo</Text>
+
+                    <img src={LeftLineImg} className="left-line-img" alt="Immagine di una riga bianca"/>
+                    <img src={RightLineImg} className="right-line-img" alt="Immagine di una riga bianca"/>
+                    <img src={CarImg} className="car-img" alt="Immagine di una macchina blu"/>
+
                 </div>
             </div>
         </section>
