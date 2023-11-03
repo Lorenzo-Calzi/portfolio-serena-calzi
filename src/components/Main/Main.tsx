975978c409f6f5144525d1d61b2953f7cf548464
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-11-03 14:50:08 +0100

    created CoursesStub

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './main.scss'
 import Jumbotron from "./Jumbotron/Jumbotron";
+import Contact from "./Contact/Contact";
 
 const Main = () => {
 
     return (
         <main id="main">
             <Jumbotron />
+            <Contact />
         </main>
     )
 }
---
+++ src/stub/CoursesSTUB.ts
@@ -0,0 +1,1 @@
+export default ["Corso 1", "Corso 2", "Corso 3", "Corso 4", "Corso 5"];
