59faa381bd97914977f50fdcc5fcce723af07a9d
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-11-08 10:31:01 +0100

    blocked scroll when popup is visible

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useSelector} from "react-redux";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
@@ -8,6 +8,15 @@ import Popup from "../reusable/Popup/Popup";
 const Page = () => {
     const { loaderVisibility } = useSelector((state: any) => state.loader)
     const { popupVisibility, popupMessage } = useSelector((state: any) => state.popup)
+
+    useEffect(() => {
+        if(popupVisibility) {
+            document.body.classList.add('scroll-disabled')
+        } else {
+            document.body.classList.remove('scroll-disabled')
+        }
+    }, [popupVisibility]);
+
 
     return (
         <div id="page">
