8f5543e2d0bfff7ef10b9d61a1b5ddf79b764926
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-11-16 17:27:09 +0100

    fixed Services component

--- src/components/Main/Services/Services.tsx
+++ src/components/Main/Services/Services.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useLayoutEffect, useState} from "react";
 import './services.scss'
 import Service from "./Service/Service";
 import Text from "../../reusable/Text/Text"
@@ -13,6 +13,58 @@ import StarBig from "../../../assets/images/star.png"
 import StarSmall from "../../../assets/images/star-sm.png"
 
 const Services = () => {
+    const [swiperSetting, setSwiperSetting] = useState({
+        slidesPerView: 4,
+        spaceBetween: 30
+    })
+
+    useLayoutEffect(() => {
+        calculateSlidesToShow();
+
+        window.addEventListener("resize", calculateSlidesToShow);
+        return () => {
+            window.removeEventListener("resize", calculateSlidesToShow);
+        };
+    }, []);
+
+    const calculateSlidesToShow = () => {
+        if (window.innerWidth <= 320) {
+            setSwiperSetting({
+                slidesPerView: 1,
+                spaceBetween: 30
+            })
+        } else if (window.innerWidth >= 321 && window.innerWidth <= 480) {
+            setSwiperSetting({
+                slidesPerView: 1,
+                spaceBetween: 30
+            })
+        } else if (window.innerWidth >= 481 && window.innerWidth <= 768) {
+            setSwiperSetting({
+                slidesPerView: 2,
+                spaceBetween: 30
+            })
+        } else if (window.innerWidth >= 769 && window.innerWidth <= 1024) {
+            setSwiperSetting({
+                slidesPerView: 3,
+                spaceBetween: 30
+            })
+        } else if (window.innerWidth >= 1025 && window.innerWidth <= 1200) {
+            setSwiperSetting({
+                slidesPerView: 3,
+                spaceBetween: 30
+            })
+        } else if (window.innerWidth >= 1201 && window.innerWidth <= 1500) {
+            setSwiperSetting({
+                slidesPerView: 3,
+                spaceBetween: 30
+            })
+        } else {
+            setSwiperSetting({
+                slidesPerView: 4,
+                spaceBetween: 30
+            })
+        }
+    };
     return (
         <div id="services">
             <div className="page-container">
@@ -25,7 +77,7 @@ const Services = () => {
                         <Text type={"h2"} textAlign={"center"}>Servizi</Text>
                     </motion.div>
 
-                    <Carousel stub={ServicesSTUB}>
+                    <Carousel slidesPerView={swiperSetting.slidesPerView} spaceBetween={swiperSetting.spaceBetween} stub={ServicesSTUB}>
                         {
                             ServicesSTUB.map((service, index) => (
                                 <SwiperSlide key={index}>
