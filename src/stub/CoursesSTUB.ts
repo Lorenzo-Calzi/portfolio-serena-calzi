9abe79a6c9fadf648e702f920ca5431cec965fc0
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-11-16 17:27:27 +0100

    fixed CarouselSTUB

--- src/stub/CoursesSTUB.ts
+++ src/stub/CoursesSTUB.ts
@@ -1,1 +1,42 @@
-export default ["Corso 1", "Corso 2", "Corso 3", "Corso 4", "Corso 5"];
+export default [
+    {
+        title: "Corso 1",
+        description: "Interactively brand client center through is customized value good ideas.",
+        age: "0-2 anni",
+        time: "10-12 am",
+        image: "",
+        price: "€47"
+    },
+    {
+        title: "Corso 2",
+        description: "Interactively brand client center through is customized value good ideas.",
+        age: "2-5 anni",
+        time: "12-13 am",
+        image: "",
+        price: "€15"
+    },
+    {
+        title: "Corso 3",
+        description: "Interactively brand client center through is customized value good ideas.",
+        age: "5-10 anni",
+        time: "14-16 pm",
+        image: "",
+        price: "€32"
+    },
+    {
+        title: "Corso 4",
+        description: "Interactively brand client center through is customized value good ideas.",
+        age: "10-15 anni",
+        time: "16-18 pm",
+        image: "",
+        price: "€50"
+    },
+    {
+        title: "Corso 5",
+        description: "Interactively brand client center through is customized value good ideas.",
+        age: "15-20 anni",
+        time: "18-20 pm",
+        image: "",
+        price: "€25"
+    }
+];
