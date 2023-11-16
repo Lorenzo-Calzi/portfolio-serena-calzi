f84046208055376ccd46dafb18f28dc8177c0204
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-11-16 10:08:16 +0100

    added libraries

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import './main.scss'
 import Jumbotron from "./Jumbotron/Jumbotron";
-import Counter from "./Counter/Counter";
+import Services from "./Services/Services";
 import Contact from "./Contact/Contact";
+import Counter from "./Counter/Counter";
 
 const Main = () => {
-
     return (
         <main id="main">
             <Jumbotron />
+            <Services />
             <Counter />
             <Contact />
         </main>
