f6383a0a2f805f0faefb951abceb7660553568d8
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-12-01 16:11:48 +0100

    created Steps and Step component, made it responsive, created StepsSTUB and added images

---
+++ src/stub/StepsSTUB.ts
@@ -0,0 +1,21 @@
+import ImageStep1 from "../assets/images/step-1.png"
+import ImageStep2 from "../assets/images/step-2.png"
+import ImageStep3 from "../assets/images/step-3.png"
+
+export default [
+    {
+        image: ImageStep1,
+        title: "Expert Instructor",
+        description: "ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+    },
+    {
+        image: ImageStep2,
+        title: "Known Everything",
+        description: "ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+    },
+    {
+        image: ImageStep3,
+        title: "Well Children",
+        description: "ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+    }
+]
