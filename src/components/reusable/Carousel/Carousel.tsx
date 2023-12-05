0e49fba9ce4b2cb4ddb3a0ecde6ab4b35bffd648
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-12-05 16:06:34 +0100

    fixed carousel component

--- src/components/reusable/Carousel/Carousel.tsx
+++ src/components/reusable/Carousel/Carousel.tsx
@@ -1,39 +1,45 @@
 import React, {useRef} from "react";
 import './carosuel.scss'
 import {Swiper} from "swiper/react";
-import {Navigation} from 'swiper/modules';
+import {Autoplay, Navigation, Pagination} from 'swiper/modules';
 import {Swiper as SwiperType} from 'swiper';
 
 interface CarouselProps {
     children: React.ReactNode,
     slidesPerView: number,
     spaceBetween: number,
+    navigation?: boolean
+    pagination?: boolean,
+    autoplay?: boolean,
     stub: {}[]
 }
 
-const Carousel = ({children, stub, slidesPerView, spaceBetween}: CarouselProps) => {
+const Carousel = ({children, stub, slidesPerView, navigation, pagination, autoplay, spaceBetween}: CarouselProps) => {
     const swiperRef = useRef<SwiperType>();
 
     return (
         <div id="carousel">
+            { stub.length > slidesPerView && navigation && <div onClick={() => swiperRef.current?.slidePrev()} className="arrow-left" /> }
+
             <Swiper slidesPerView={slidesPerView}
                     spaceBetween={spaceBetween}
                     onBeforeInit={(swiper: any) => {
                         swiperRef.current = swiper;
                     }}
                     loop
-                    modules={[Navigation]}>
+                    autoplay={autoplay && {
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={pagination && {
+                        clickable: true,
+                        dynamicBullets: true,
+                    }}
+                    modules={[Autoplay, Navigation, Pagination]}>
                 {children}
             </Swiper>
 
-            {
-                stub.length > slidesPerView && (
-                    <div className="arrows">
-                        <div onClick={() => swiperRef.current?.slidePrev()} className="arrow-left" />
-                        <div onClick={() => swiperRef.current?.slideNext()} className="arrow-right" />
-                    </div>
-                )
-            }
+            { stub.length > slidesPerView && navigation &&  <div onClick={() => swiperRef.current?.slideNext()} className="arrow-right" /> }
         </div>
     )
 }
