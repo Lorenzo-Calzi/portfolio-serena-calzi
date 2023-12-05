fdd27d63782e3f0ad3ba544552501269a51fbea7
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-12-05 16:07:15 +0100

    created About component, made it responsive, added images and added AboutSTUB

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,12 +6,14 @@ import Contact from "./Contact/Contact";
 import Counter from "./Counter/Counter";
 import Courses from "./Courses/Courses";
 import Steps from "./Steps/Steps";
+import About from "./About/About";
 
 const Main = () => {
     return (
         <main id="main">
             <Jumbotron />
             <Services />
+            <About />
             <Counter />
             <Courses />
             <Steps />
---
+++ src/stub/AboutSTUB.ts
@@ -0,0 +1,23 @@
+import Image1 from "../assets/icons/image-1.svg";
+import Image2 from "../assets/icons/image-2.svg";
+import Image3 from "../assets/icons/image-3.svg";
+import Image4 from "../assets/icons/image-4.svg";
+
+export default  [
+    {
+        image: Image1,
+        backgroundColor: '#ff977f',
+    },
+    {
+        image: Image2,
+        backgroundColor: '#8b80ff',
+    },
+    {
+        image: Image3,
+        backgroundColor: '#ffd57d',
+    },
+    {
+        image: Image4,
+        backgroundColor: '#80fff3',
+    }
+]
