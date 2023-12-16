9d94337b53797ec47217f7dbcdc953c52a41d0fe
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-12-16 15:58:03 +0100

    added Button animation e fixed responsive

--- src/components/reusable/Button/Button.tsx
+++ src/components/reusable/Button/Button.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './button.scss'
-import Text from '../Text/Text'
 import Anchor from "../Anchor/Anchor";
 
 interface ButtonProps {
@@ -12,21 +11,26 @@ interface ButtonProps {
     isDisabled?: boolean
 }
 const Button = ({text, handler, type, link, className, isDisabled}: ButtonProps) => {
-
     return (
-        <button className={`custom-button ${className && className}`}
-                onClick={handler ? handler : undefined}
-                type={type ? type : 'button'}
-                disabled={isDisabled ? isDisabled : false}
-        >
-            {
-                !link ? (
-                    <Text type={"p-big"} color={"white"} textAlign={'center'}>{text}</Text>
-                ) : (
-                    <Anchor href={link} color={"white"}>{text}</Anchor>
-                )
-            }
-        </button>
+        link ? (
+            <Anchor href={link} color={"white"}>
+                <button className={`custom-button ${className && className}`}
+                        onClick={handler ? handler : undefined}
+                        type={type ? type : 'button'}
+                        disabled={isDisabled ? isDisabled : false}>
+                    {text}
+                    <span/><span/><span/><span/>
+                </button>
+            </Anchor>
+        ) : (
+            <button className={`custom-button ${className && className}`}
+                    onClick={handler ? handler : undefined}
+                    type={type ? type : 'button'}
+                    disabled={isDisabled ? isDisabled : false}>
+                {text}
+                <span/><span/><span/><span/>
+            </button>
+        )
     )
 }
 
