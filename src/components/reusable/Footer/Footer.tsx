0ee93cd14e4864785f1f633caef3b9dfbffb5c2f
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-12-22 11:18:05 +0100

    changed Footer text

--- src/components/reusable/Footer/Footer.tsx
+++ src/components/reusable/Footer/Footer.tsx
@@ -8,8 +8,11 @@ const Footer = () => {
             <div className="page-container">
                 <Text type={"p-small"}
                       textAlign={"center"}
-                      color={"#888888FF"}>
-                    Copyright 2023 By Serena Calzi | All Right Reserved.
+                      color={"#888888FF"}
+                >
+                    Copyright © 2023 By <a href="https://portfolio-lorenzo-calzi.vercel.app/" target="_blank">Lorenzo
+                    Calzi</a> | All
+                    Right Reserved.
                 </Text>
             </div>
         </footer>
