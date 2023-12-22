39849e51a02ca9488aa35d1d9755bd933e5946c7
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2023-12-22 12:08:11 +0100

    added AboutPage Description

--- src/components/Aboutpage/AboutPage.tsx
+++ src/components/Aboutpage/AboutPage.tsx
@@ -3,11 +3,13 @@ import "./aboutPage.scss"
 import Footer from "../reusable/Footer/Footer";
 import Header from "../reusable/Header/Header";
 import Breadcrumb from "../reusable/Breadcrumb/Breadcrumb";
+import Description from "./Description/Description";
 
 const AboutPage = () => {
     return (
         <div>
             <Header components={<Breadcrumb title={"About"}/>}/>
+            <Description/>
             <Footer/>
         </div>
     )
