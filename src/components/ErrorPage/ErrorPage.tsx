ad28cc3eb04b0133e17b62fd57216278ac523e56
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2024-01-10 10:28:47 +0100

    added ErrorPage content and images

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import './errorPage.scss'
+import Text from "../reusable/Text/Text"
+import Button from "../reusable/Button/Button";
 
 const ErrorPage = () => {
     return (
-        <div id="errorPage">
-            <h1>404</h1>
+        <div id="error-page">
+            <div className="page-container">
+                <div className="content">
+                    <div className="copies">
+                        <Text type={"h2"} color={"#b5bfff0d"}>404 Error</Text>
+                        <Text type={"h1"} color={"white"}>Pagina Non Trovata</Text>
+                        <Text type={"h4"} color={"white"}>Purtroppo qualcosa è andato storto e questa pagina non esiste.
+                            Prova a utilizzare la ricerca o torna alla pagina precedente.</Text>
+                    </div>
+
+                    <Button text={"Torna alla home"} link={"/"}/>
+                </div>
+            </div>
         </div>
     )
 }
