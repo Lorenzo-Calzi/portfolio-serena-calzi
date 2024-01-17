50487a603a4fbf306e049a4d583e30c96783fe2f
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2024-01-17 09:52:30 +0100

    first setup backend, setup CRUD

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config({path: "./config.env"});
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI;
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts")
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use(workoutRoutes)
+
+// connect to db
+mongoose.connect(uri)
+    .then(() => {
+        app.listen(port, () => {
+            console.log("Connected to db & listening on port", port)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
